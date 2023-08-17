8d409fedcbbfc3675a15279e56a784bf9d93895b
midnightconman <midnightconman@gmail.com>
2023-08-17 11:21:31 -0700

    More API Schema Updates

--- scrape.sh
+++ scrape.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 # Write index db
-curl -sSfL -o db.json https://vuln.go.dev/index/db.json
+curl -sSfL -o index/db.json https://vuln.go.dev/index/db.json
 
 # Write index modules
-curl -sSfL -o modules.json https://vuln.go.dev/index/modules.json
-_IDS=$( cat modules.json | jq -r '.[].vulns[].id' | sort | uniq )
+curl -sSfL -o index/modules.json https://vuln.go.dev/index/modules.json
+_IDS=$( cat index/modules.json | jq -r '.[].vulns[].id' | sort | uniq )
 
 # Write index vulns
-curl -sSfL -o vulns.json https://vuln.go.dev/index/vulns.json
+curl -sSfL -o index/vulns.json https://vuln.go.dev/index/vulns.json
 
 mkdir -p ID
 for i in $_IDS ; do
